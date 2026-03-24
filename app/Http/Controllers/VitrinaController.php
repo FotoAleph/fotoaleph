ec6fb560de5f8f177e88a2ae5b3a86e7b78fb5ae
Carlos Ramirez <fotoaleph@gmail.com>
2026-03-24 11:19:19 -0500

    vitrina

---
+++ app/Http/Controllers/VitrinaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vitrina;
+use Illuminate\Http\Request;
+
+class VitrinaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vitrina $vitrina)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vitrina $vitrina)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vitrina $vitrina)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vitrina $vitrina)
+    {
+        //
+    }
+}
