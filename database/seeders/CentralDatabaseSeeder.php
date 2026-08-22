bcbfab3ffe42badee8b8354978c14c134b1b5be3
charly A. <fotoaleph@gmail.com>
2026-08-22 14:15:50 -0500

    correccion en seedes roles y usuarios

--- database/seeders/CentralDatabaseSeeder.php
+++ database/seeders/CentralDatabaseSeeder.php
@@ -11,28 +11,28 @@ class CentralDatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
+        User::updateOrCreate(['email' => 'test@dinamycode.com'], [
             'name' => 'Test User',
             'email' => 'test@dinamycode.com',
             'role' => 'cliente',
             'password' => bcrypt('dinamycodeDC*'),
         ]);
 
-        User::factory()->create([
+        User::updateOrCreate(['email' => 'admin@dinamycode.com'], [
             'name' => 'Admin User',
             'email' => 'admin@dinamycode.com',
             'role' => 'admin',
             'password' => bcrypt('dinamycodeDC*'),
         ]);
 
-        User::factory()->create([
+        User::updateOrCreate(['email' => 'fotoaleph@dinamycode.com'], [
             'name' => 'Carlos Alberto Ramirez',
             'email' => 'fotoaleph@dinamycode.com',
             'role' => 'coordinador',
             'password' => bcrypt('dinamycodeDC*'),
         ]);
 
-        User::factory()->create([
+        User::updateOrCreate(['email' => 'sportbogotafc@gmail.com'], [
             'name' => 'Sport Bogota',
             'email' => 'sportbogotafc@gmail.com',
             'role' => 'coordinador',
